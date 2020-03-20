b24a80cde4ef2e55ba67ae8d7747a6025d776d59
David White <dwhite0627@gmail.com>
2020-03-20 19:31:22 -0400

    Add the PrivateRoute component

---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const token = localStorage.getItem("token");
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        const path = token ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        );
+        return path;
+      }}
+    />
+  );
+};
+
+export const PrivateRoute;
